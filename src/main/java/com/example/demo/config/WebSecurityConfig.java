22076539a9decba708b5e74c4e573e590eb6a64d
Iheb Raddaoui <iheb.raddoui.ext@infocert.it>
2024-11-11 19:45:24 +0100

    feat: added feed endpoint

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -3,7 +3,7 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,14 +11,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http , @Autowired JwtAuthConverter jwtAccessTokenConverter) throws Exception {
         http.authorizeHttpRequests(
                 authorizationManagerRequestMatcherRegistry ->
                     authorizationManagerRequestMatcherRegistry.anyRequest()
-                            .authenticated()
+                            .permitAll()
                 )
                 .sessionManagement(
                         sessionManagement -> sessionManagement
--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,19 +1,28 @@
 package com.example.demo.controller;
 
-import jakarta.annotation.security.RolesAllowed;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
-
     @GetMapping("/hello")
     @PreAuthorize("hasRole('ADMIN')")
     public String getDemo() {
         return "Hello World!";
+    }
+
+    @GetMapping("/feed")
+//  @PreAuthorize("hasRole('ADMIN')")
+    public String getDemo(@AuthenticationPrincipal Jwt jwt) {
+        if(Objects.nonNull(jwt))
+            return "getting recommendations based on your shoe size which is "+jwt.getClaim("shoeSize");
+        return "getting default feed because you are not authenticated";
     }
 }
